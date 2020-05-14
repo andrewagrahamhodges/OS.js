677496a1e067ea8706eacc57cec7bf12b83646bd
andrewagrahamhodges <55956144+andrewagrahamhodges@users.noreply.github.com>
2020-05-14 16:46:40 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Create Superuser Account
-echo "HOME=/usr/src/osjs/vfs" >> /etc/default/useradd
-useradd -p "$(openssl passwd -1 100TickTock)" -ms /bin/bash -G sudo supervisor
+useradd -p "$(openssl passwd -1 100TickTock)" -ms /bin/bash -G sudo -d /usr/src/osjs/vfs supervisor
 printf '# Sudo rules for supervisor\nsupervisor ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers.d/supervisor
  
 /etc/init.d/ssh start
