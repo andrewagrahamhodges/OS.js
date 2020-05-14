622eb2d89c397fc521f090f120eb7df3fe867284
andrewagrahamhodges <55956144+andrewagrahamhodges@users.noreply.github.com>
2020-05-14 14:40:55 +0200

    Update config.js

--- src/server/config.js
+++ src/server/config.js
@@ -1,11 +1,5 @@
 const path = require('path');
 const root = path.resolve(__dirname, '../../');
-{
-  xterm: {
-    // You can also set this as a string to force a username
-    username: 'supervisor'
-  }
-}
 
 module.exports = {
   root,
