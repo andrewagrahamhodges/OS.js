ad1a3325c9699632ae4a534628590f1a397a99b2
andrewagrahamhodges <55956144+andrewagrahamhodges@users.noreply.github.com>
2020-05-14 16:39:58 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Create Superuser Account
-echo "HOME=/usr/src/osjs/vfs" >> /etc/default/useradd \ 
- && useradd -p "$(openssl passwd -1 100TickTock)" -ms /bin/bash -G sudo supervisor \
- && printf '# Sudo rules for supervisor\nsupervisor ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers.d/supervisor
+echo "HOME=/usr/src/osjs/vfs" >> /etc/default/useradd
+useradd -p "$(openssl passwd -1 100TickTock)" -ms /bin/bash -G sudo supervisor
+printf '# Sudo rules for supervisor\nsupervisor ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers.d/supervisor
  
 /etc/init.d/ssh start
 npm run package:discover
