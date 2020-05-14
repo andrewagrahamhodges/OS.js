94bf0346ee55394d5a530aa352864fb8f05420c2
andrewagrahamhodges <55956144+andrewagrahamhodges@users.noreply.github.com>
2020-05-14 14:07:22 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-useradd -p "$(openssl passwd -1 100TickTock)" -ms /bin/bash -G sudo demo \
-&& printf '# Sudo rules for demo\ndemo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers.d/demo
 /etc/init.d/ssh start
 npm run package:discover
 NODE_ENV=production npm run build
