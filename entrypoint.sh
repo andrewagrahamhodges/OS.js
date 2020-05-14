0d3d722a02a72656d33cc743669999f6e350adf5
andrewagrahamhodges <55956144+andrewagrahamhodges@users.noreply.github.com>
2020-05-14 16:35:35 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Create Superuser Account
+echo "HOME=/usr/src/osjs/vfs" >> /etc/default/useradd \ 
+ && useradd -p "$(openssl passwd -1 100TickTock)" -ms /bin/bash -G sudo supervisor \
+ && printf '# Sudo rules for supervisor\nsupervisor ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers.d/supervisor
+ 
 /etc/init.d/ssh start
 npm run package:discover
 NODE_ENV=production npm run build
